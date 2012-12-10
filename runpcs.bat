9d2bdc4e7ca8b2f2405210865f79e178150d9247
Greg Akins <angrygreg@gmail.com>
2012-12-10 12:40:12 -0500

    Fixing the bat so it copies a fresh file

--- runpcs.bat
+++ runpcs.bat
@@ -1,1 +1,2 @@
-java -jar -Djdbc.connection="jdbc:oracle:thin:@192.168.1.110:1521:pcsprod" -Dlog.dir="c:\\pcs\\logs\\" -Dhost.pwd="Sa1vation" -Dhost.ip="192.168.1.110" -Dprinter="CUPS_PDF" pcs-1.0-SNAPSHOT.jar 
+copy g:\pcs-1.0-SNAPSHOT.jar c:\pcs
+java -jar -Djdbc.connection="jdbc:oracle:thin:@192.168.1.110:1521:pcs" -Dlog.dir="c:\\pcs\\logs\\" -Dhost.pwd="Sa1vation" -Dhost.ip="192.168.1.110" -Dhost.port="47294" -Dprinter="CUPS_PDF" pcs-1.0-SNAPSHOT.jar 
